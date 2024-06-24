f12c207521818d11f51df4d26466578e8e61adc5
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-06-24 21:16:50 +0200

    rename code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use chaotic_networks::category::topology::GrothendieckTopology;
-use chaotic_networks::graph::chip_firing::ChipFiringNode;
-use chaotic_networks::graph::graph::Graph;
-use chaotic_networks::graph::hopfield::HopfieldNode;
-use chaotic_networks::graph::node::Node;
-use chaotic_networks::sheaf::conditions::{HopfieldStateCondition, SumCondition};
-use chaotic_networks::simulation::chip_firing::simulate_chip_firing;
-use chaotic_networks::simulation::hopfield::simulate_hopfield_network;
+use divisor_networks::category::topology::GrothendieckTopology;
+use divisor_networks::graph::chip_firing::ChipFiringNode;
+use divisor_networks::graph::graph::Graph;
+use divisor_networks::graph::hopfield::HopfieldNode;
+use divisor_networks::graph::node::Node;
+use divisor_networks::sheaf::conditions::{HopfieldStateCondition, SumCondition};
+use divisor_networks::simulation::chip_firing::simulate_chip_firing;
+use divisor_networks::simulation::hopfield::simulate_hopfield_network;
 
 fn main() {
     let max_iterations = 1000;
